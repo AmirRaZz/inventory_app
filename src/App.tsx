d36c60e6bb27ecdc4f7afbf016b861f59df48c18
RaZz <amirrahmati471@gmail.com>
2025-04-06 18:45:30 +0330

    Implement product filtering and sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,70 @@
 import NavBar from "./components/NavBar";
 import CategoryForm from "./components/Category";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Category } from "./types/CategoryType";
 import ProductsForm from "./components/Products";
 import ProductsList from "./components/ProductList";
 import { Product } from "./types/ProductType";
-
-
+import Filter from "./components/Filter";
 
 function App() {
   const [categories, setCategories] = useState<Category[]>([]);
-    const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const [sort, setSort] = useState("latest");
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    let result = products;
+    result = filterSearchTitle(result);
+    result = sortDate(result);
+    setFilteredProducts(result);
+  }, [products, sort, searchValue]);
+
+  const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value.trim().toLowerCase());
+  };
+
+  const sortHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSort(e.target.value);
+  };
+
+  const filterSearchTitle = (array: Product[]) => {
+    return array.filter((product) =>
+      product.title.toLowerCase().includes(searchValue)
+    );
+  };
+
+  const sortDate = (array: Product[]) => {
+    const sortedProducts = [...array];
+    sortedProducts.sort((a, b) => {
+      if (sort === "latest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+      } else if (sort === "earliest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+      }
+      return 0; // Default case to satisfy TypeScript
+    });
+    return sortedProducts;
+  };
+
   return (
     <div className="bg-slate-800 min-h-screen">
       <NavBar />
       <div className="container max-w-xl mx-auto p-4">
         <CategoryForm setCategories={setCategories} />
         <ProductsForm categories={categories} setProducts={setProducts} />
-        <ProductsList products={products} categories={categories} setProducts={setProducts} />
+        <Filter
+          sort={sort}
+          searchValue={searchValue}
+          onSort={sortHandler}
+          onSearch={searchHandler}
+        />
+        <ProductsList
+          products={filteredProducts}
+          categories={categories}
+          setProducts={setProducts}
+        />
       </div>
     </div>
   );
